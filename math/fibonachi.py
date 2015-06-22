932a476d013f7709bbf84117d0c5c338ad282b13
Robert Welin-Berger <avacore1337@gmail.com>
2015-06-22 22:16:14 +0200

    solved it but it overflows...

--- math/fibonachi.py
+++ math/fibonachi.py
@@ -3,18 +3,26 @@
 
 from numpy import *
 
-def power(matrix, n):
-    pass
+def power(m, n):
+    print(n)
+    if n == 1:
+        return m
+    r = power(m, math.floor(n/2))
+    r = r*r
+    if n%2 == 1:
+        r = r*m
+    return r
 
 def fibonacciNumber(n):
-    matrix = matrix('1 1; 1 0')
-    print(matrix)
+    m = matrix('1 1; 1 0')
+    print(m)
     if n == 0:
         return 0
-    
+    r = power(m, n - 1)
+    return r.A[0][0]
 
 def main():
-    n = input("what fibonacci number do you want?")
+    n = int(input("what fibonacci number do you want?"))
     print(fibonacciNumber(n))
 
 if __name__ == '__main__':
